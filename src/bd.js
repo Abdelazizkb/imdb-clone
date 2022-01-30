621a425e34e6a8d4dd7816262c8fee7a618d8247
Abdelazizkb <korbaabdo@gmail.com>
2022-01-30 17:25:57 +0100

    pic size

--- src/bd.js
+++ src/bd.js
@@ -2,43 +2,48 @@ import pic1 from './images/pic1.png'
 import pic2 from './images/pic2.png'
 import pic3 from './images/pic3.png'
 import pic4 from './images/pic4.png'
+import pic5 from './images/pic5.png'
+import pic6 from './images/pic6.png'
+import pic7 from './images/pic7.png'
+import pic8 from './images/pic8.png'
+import pic9 from './images/pic9.png'
 
 export const CarouselData=[
     { 
       image:"https://i.ytimg.com/vi/ANpDikx-xc0/maxresdefault.jpg",
-      thumb:"https://dzawaya.com/wp-content/uploads/2021/04/aad7cd3dce01d806c5d347ffdaf8fa9e.jpg",
+      thumb:pic9,
       title:"Achour 10",
       rate:"9",  
     },
     {   image:"https://img.over-blog-kiwi.com/3/14/17/67/20190511/ob_9c48b6_21.jpg",
-        thumb:"https://yt3.ggpht.com/ytc/AKedOLQtM-Jd2TKuteI8NjrbSdHkm3j7x4apY1eNWmlIVQ=s900-c-k-c0x00ffffff-no-rj",
+        thumb:pic8,
         title:"Dakyous && Makyous",
         rate:"8.5",    
     },
     {   image:"https://cdn.liberte-algerie.com/images/article/thumbs/d-linoubliable-inspecteur-tahar-d6281.jpg",
-        thumb:"https://m.media-amazon.com/images/M/MV5BODRjZjg1OTUtZWM2Ny00NTRkLWE3ODktYmZjOWNkNDlmMjRjXkEyXkFqcGdeQXVyNTIyODg3NDM@._V1_.jpg",
+        thumb:pic7,
         title:"Inspecteur tahar",
         rate:"9.5",      
     },
     {   image:"https://dia-algerie.com/wp-content/uploads/2019/04/corso.jpg",
-        thumb:"https://pbs.twimg.com/media/DbgOIkaXkAIeXXv.jpg",
+        thumb:pic6,
         title:"Rais corso",
         rate:"1.5",      
     },
 
     {   image:"https://i.ytimg.com/vi/bdDoVmOc8jU/maxresdefault.jpg",
-        thumb:"https://1.bp.blogspot.com/-pDPAEXMRcXg/XQOen0U7eDI/AAAAAAAA3fI/_NkpE_jPogkGYMYs0DxwO-aixmSH1XaMQCLcBGAs/s1600/14%2BBou-Baltou%2B2.jpg",
+        thumb:pic5,
         title:"Bou balto (lah ghaleb n3amero bih el vide) ",
         rate:"0.5",        
     },
     { 
         image:"https://i.ytimg.com/vi/ANpDikx-xc0/maxresdefault.jpg",
-        thumb:"https://dzawaya.com/wp-content/uploads/2021/04/aad7cd3dce01d806c5d347ffdaf8fa9e.jpg",
+        thumb:pic9,
         title:"Achour 10",
         rate:"9",  
     },
     {   image:"https://img.over-blog-kiwi.com/3/14/17/67/20190511/ob_9c48b6_21.jpg",
-          thumb:"https://yt3.ggpht.com/ytc/AKedOLQtM-Jd2TKuteI8NjrbSdHkm3j7x4apY1eNWmlIVQ=s900-c-k-c0x00ffffff-no-rj",
+          thumb:pic8,
           title:"Dakyous && Makyous",
           rate:"8.5",    
     },
